aafeef405dfe73b49447ca70b838d7ad0f203d1e
Fernando Valdes <fvaldes@wearegennius.com>
2025-05-22 02:14:04 -0300

    fix: vercel conf

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/next';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Component {...pageProps} />
+    <SpeedInsights />
+    <Analytics />
+    </>
+  );
 }
 
 export default MyApp
